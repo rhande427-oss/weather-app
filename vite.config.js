736be4defa4674c616019beb88fba071e5b35ba9
Rohan Hande <rhande427-oss@users.noreply.github.com>
2026-09-10 12:31:17 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/weather-app/",
   server: {
     port: 3000, // you can change the port if needed
   },
